c0ae449959338f09e871100098f5d28ba8ea8ee1
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2019-07-23 18:28:49 +0300

    Добавил пример Svelte
    Добавил пример Svelte + динамический год опыта работы

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,8 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $('#experience-year').text(new Date().getFullYear() - 2006);
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
